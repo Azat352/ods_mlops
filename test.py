3d5e6ceec02593a2686dd9a14800b77f4f0bcd6f
Azat <ahm352@gmail.com>
2024-07-01 00:32:14 +0300

    update pre-commit-config

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
-def print_test():
-    print(
-        "test printing"
-    )
+def print_test() -> None:
+    print("test printing")
 
 
 print(
     "hhdlsfjlsdkfj;lakdjfkjadlfkjsdlfjdsfdkjfdldfjsdlkjflkdjfslfksjjfkdjflakdjflkajd;flkad;flkjad;lfk"
 )
 
-print(
-    "12331j2k"
-)
-my = (
-    Class()
-    .fit()
-    .predict()
-    .ygnfg()
-    .fajhldksfjhalkdjfhuyoqurehqfbadjfakbajjaaaaaajjnnnnnadnfandfnanfn()
-)
+print("12331j2k")
