75e24a855dfe356eb7c1d3c0378e5674a40fe140
Azat <ahm352@gmail.com>
2024-06-30 23:30:24 +0300

    add project config files

---
+++ test.py
@@ -0,0 +1,20 @@
+def print_test():
+    print(
+        "test printing"
+    )
+
+
+print(
+    "hhdlsfjlsdkfj;lakdjfkjadlfkjsdlfjdsfdkjfdldfjsdlkjflkdjfslfksjjfkdjflakdjflkajd;flkad;flkjad;lfk"
+)
+
+print(
+    "12331j2k"
+)
+my = (
+    Class()
+    .fit()
+    .predict()
+    .ygnfg()
+    .fajhldksfjhalkdjfhuyoqurehqfbadjfakbajjaaaaaajjnnnnnadnfandfnanfn()
+)
